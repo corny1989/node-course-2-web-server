d0d778aa164702eae952ff97f0eb15ef17183d4a
corny1989 <jan@walari.de>
2018-03-03 17:19:31 +0100

    Setup Startscript & heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -71,6 +73,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(8080, () => {
-    console.log("Server is up on Port 8080");
+app.listen(port, () => {
+    console.log(`Server is up on Port ${port}`);
 });
